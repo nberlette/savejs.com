e9b2231783ba5dbb5bb6fb7d499807e659f8d070
Nicholas Berlette <nick@berlette.com>
2021-10-25 17:16:26 -0700

    🔨  typescript support

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import App from './App.svelte';
+
+export default new App({
+	target: document.body,
+	props: {
+		name: 'savejs.com',
+		repository: 'nberlette/savejs.com',
+		baseUrl: 'https://savejs.com',
+	}
+});
+
